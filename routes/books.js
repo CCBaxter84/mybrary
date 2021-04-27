7146a24c92ef7021bed38e5e88d6ed74177f00c7
CCBaxter84 <troyceclark@gmail.com>
2021-04-27 16:12:34 -0600

    Completed Recently Added Books

--- routes/books.js
+++ routes/books.js
@@ -15,8 +15,28 @@ const upload = multer({
 
 // @route GET /
 // @desc  Get all books and render to view
-router.get("/", (req, res) => {
-  res.send("All Books")
+router.get("/", async (req, res) => {
+  let query = Book.find();
+  console.log(req.query);
+  if (req.query.title != null && req.query.title != "") {
+    query = query.regex("title", new RegExp(req.query.title, "i"));
+  }
+  if (req.query.publishedBefore != null && req.query.publishedBefore != "") {
+    query = query.lte("publishDate", req.query.publishedBefore);
+  }
+  if (req.query.publishedAfter != null && req.query.publishedAfter != "") {
+    query = query.gte("publishDate", req.query.publishedAfter);
+  }
+
+  try {
+    const books = await query.exec();
+    res.render("books/index", {
+      books,
+      searchOptions: req.query
+    });
+  } catch {
+    res.redirect("/");
+  }
 });
 
 // @route GET /new

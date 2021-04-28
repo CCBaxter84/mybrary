3bc4c7d3b794691be3fa2520913c4d8a57094459
CCBaxter84 <troyceclark@gmail.com>
2021-04-28 17:53:38 -0600

    Finished book routes

--- routes/books.js
+++ routes/books.js
@@ -30,9 +30,34 @@ router.get("/", async (req, res) => {
 });
 
 // @route GET /new
-// @desc  Render form to view for adding a new book
+// @desc  Render form for adding a new book to view
 router.get("/new", async (req, res) => {
   renderNewPage(res, new Book());
+});
+
+// @route GET /:id
+// @desc  Render view of all info related to a specific book
+router.get("/:id", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id)
+                            .populate("author")
+                            .exec();
+    res.render("books/show", { book });
+  } catch {
+    res.redirect("/")
+  }
+});
+
+// @route GET /:id/edit
+// @desc  Render page for editing existing info of an existing book
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    renderEditPage(res, book);
+  } catch {
+    res.redirect("/");
+  }
+
 });
 
 // @route POST /
@@ -48,21 +73,77 @@ router.post("/", async (req, res) => {
   saveCover(book, req.body.cover);
   try {
     const newBook = await book.save();
-    // res.redirect(`books/${newBook.id})
-    res.redirect("books");
+    res.redirect(`books/${newBook.id}`);
   } catch {
     renderNewPage(res, book, true);
   }
 });
 
+// @route PUT /:id
+// @desc  Update an existing book and save update to database
+router.put("/:id", async (req, res) => {
+  let book;
+  try {
+    book = await Book.findById(req.params.id);
+    book.title = req.body.title;
+    book.author = req.body.author;
+    book.publishDate = new Date(req.body.publishDate);
+    book.pageCount = req.body.pageCount;
+    book.description = req.body.description;
+    if (req.body.cover != null && req.body.cover != "") {
+      saveCover(book, req.body.cover);
+    }
+    await book.save();
+    res.redirect(`/books/${book.id}`);
+  } catch {
+    if (book != null) {
+      renderEditPage(res, book, true);
+    } else {
+      res.redirect("/");
+    }
+  }
+});
+
+// @route DELETE /:id
+// @desc  Remove a book from the database
+router.delete("/:id", async (req, res) => {
+  let book;
+  try {
+    book = Book.findById(req.params.id);
+    await book.deleteOne();
+    res.redirect("/books");
+  } catch {
+    if (book != null) {
+      res.render("books/show", {
+        book: book,
+        errorMessage: "Could not remove book"
+      });
+    } else {
+      res.redirect("/");
+    }
+  }
+});
+
 async function renderNewPage(res, book, hasError = false) {
+  renderFormPage(res, book, "new", hasError);
+}
+
+async function renderEditPage(res, book, hasError = false) {
+  renderFormPage(res, book, "edit", hasError);
+}
+
+async function renderFormPage(res, book, form, hasError = false) {
   try {
     const authors = await Author.find();
     const params = { authors, book };
     if (hasError) {
-      params.errorMessage = "Error Creating Book";
+      if (form === "edit") {
+        params.errorMessage = "Error Updating Book";
+      } else {
+        params.errorMessage = "Error Creating Book";
+      }
     }
-    res.render("books/new", params);
+    res.render(`books/${form}`, params);
   } catch {
     res.redirect("/books");
   }
